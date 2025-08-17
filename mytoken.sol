f002c61e667e8868038cbe16586c123195a739a2
Hardik Bishnoi <bishnoihardik29@gmail.com>
2025-08-17 10:38:12 +0530

    Update mytoken.sol

--- mytoken.sol
+++ mytoken.sol
@@ -1,42 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-/*
-       REQUIREMENTS
-    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
-    2. Your contract will have a mapping of addresses to balances (address => uint)
-    3. You will have a mint function that takes two parameters: an address and a value. 
-       The function then increases the total supply by that number and increases the balance 
-       of the “sender” address by that amount
-    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
-       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
-       and from the balance of the “sender”.
-    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
-       to the amount that is supposed to be burned.
-*/
-
+/// @title MyToken - A simple mintable & burnable token
+/// @author Hardik
+/// @notice This contract implements a basic token with minting and burning functionality.
+/// @dev Not ERC20 compliant, just a simple custom token for learning purposes.
 contract MyToken {
 
-    // public variables here
-    string public token_name = "Chandigarh";
-    string public token_abbv = "chd";
-    uint public total_supply = 0;
+    // Public variables
+    string public tokenName = "Chandigarh";
+    string public tokenAbbrv = "CHD";
+    uint public totalSupply = 0;
 
-    // mapping variable here
+    // Mapping from addresses to balances
     mapping(address => uint) public balances;
 
-    // mint function
-    function mint(address _to ,uint _amount)public{
-      total_supply += _amount;
-      balances[_to] += _amount;
+    /// @notice Mint new tokens to a specific address
+    /// @param _to The address to receive the tokens
+    /// @param _amount The number of tokens to mint
+    function mint(address _to, uint _amount) public {
+        totalSupply += _amount;
+        balances[_to] += _amount;
     }
 
-    // burn function
-    function burn(address _to ,uint _amount)public {
-      if(balances[_to] >= _amount){
-        total_supply -= _amount;
-        balances[_to] -= _amount;
-      }
+    /// @notice Burn tokens from a specific address
+    /// @param _from The address whose tokens will be burned
+    /// @param _amount The number of tokens to burn
+    function burn(address _from, uint _amount) public {
+        require(balances[_from] >= _amount, "Insufficient balance to burn");
+        totalSupply -= _amount;
+        balances[_from] -= _amount;
     }
-
 }
